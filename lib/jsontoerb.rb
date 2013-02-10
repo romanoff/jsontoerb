88efa840390bc54c09625daa4f12ee6b4973464a
Andriy Romanov <aromanov@partnerpedia.com>
2013-02-09 23:37:06 -0800

    Added functionality to support css map

--- lib/jsontoerb.rb
+++ lib/jsontoerb.rb
@@ -3,10 +3,11 @@ require 'json'
 
 class JsonToErb
 
-  def self.render_template(template_content, json_string)
+  def self.render_template(template_content, json_string, css_map = {})
     template = Erubis::Eruby.new(template_content)
     params = JSON.parse(json_string)
-    return template.result({:params => params})
+    css = lambda {|css_class| css_map[css_class] || css_class }
+    return template.result({:params => params, :css => css})
   end
 
 end
--- spec/jsontoerb_spec.rb
+++ spec/jsontoerb_spec.rb
@@ -4,4 +4,14 @@ describe "Jsontoerb" do
   it "should render template from json" do
     JsonToErb.render_template("<div><%= params['name'] %></div>", "{\"name\": \"Name\"}").should == "<div>Name</div>"
   end
+
+  it "should render template with css class from json" do
+    JsonToErb.render_template("<div class='<%= css['cssClass']%>'><%= params['name'] %></div>", "{\"name\": \"Name\"}").should == "<div class='cssClass'>Name</div>"
+  end
+
+  it "should use css map while rendering template" do
+    template = "<div class='<%= css['cssClass']%>'><%= params['name'] %></div>"
+    JsonToErb.render_template(template, "{\"name\": \"Name\"}", {"cssClass" => "a"}).should == "<div class='a'>Name</div>"      
+  end
+
 end
